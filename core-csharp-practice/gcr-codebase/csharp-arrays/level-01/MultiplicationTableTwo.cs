6c8058354fb44a62c0a62cd9bdd0729485528cec
Shyam Rai <shyamrrai0011@gmail.com>
2025-12-24 13:23:32 +0530

    [Shyam Rai] Added C#-Arrays programs {level-01}

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level-01/MultiplicationTableTwo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.arrays.level_01
+{
+    internal class MultiplicationTableTwo
+    {
+        public static void TableTwo()
+        {
+            //taking input
+            int[] table = new int[4];
+            Console.Write("Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //storing table in array
+            for (int i = 6; i < 10; i++)
+            {
+                table[i-6] = num * i;
+            }
+
+            //displaying result
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine($"{num} x {i+6} = {table[i]}");
+            }
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level-01/StoreValues.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.arrays.level_01
+{
+    internal class StoreValues
+    {
+        public static void Store() 
+        {
+            //taking input and declaring few key variable
+            int[] nums = new int[10];
+            int sum = 0;
+            int final = 0;
+            Console.WriteLine("Enter the elemets of the array : ");
+            //in the loop num is added unless it is less than one
+            //if num is less than one, loop is stoppes and output is displayed 
+            for (int i = 0; i < 10; i++) 
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num > 0)
+                {
+                    sum += num;
+                    nums[i] = num;
+                    final = i;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+            //output 
+            Console.WriteLine($"Sum of elements : {sum}");
+            for (int i = 0; i <= final; i++) 
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}

03cdf45d630e47238841ea5211227d6e9c9c9013
Shyam Rai <shyamrrai0011@gmail.com>
2025-12-30 15:59:40 +0530

    [Shyam Rai] Added first class and object program

---
+++ oops-csharp-practice/gcr-codebase/class-and-object/level-01/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BridgeLabzTraining.oops_csharp_practice.class_and_object
+{
+    internal class Employee
+    {
+        private string name = "Sam Jhonston";
+        private string id = "6829974";
+        private int salary = 90000;
+
+        public void Display()
+        {
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"ID : {this.id}");
+            Console.WriteLine($"Salary : {this.salary}");
+        }
+    }
+}

06d918085225242736676f1862985d78eb1c5cdb
Shyam Rai <shyamrrai0011@gmail.com>
2026-01-12 15:48:03 +0530

    [Shyam Rai] Added OOPS scenario based question (metal-factory-pipe-cutting)

---
+++ oops-csharp-practice/scenario-based/metal-factory-pipe-cutting/RodCuttingMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.metal_factory_pipe_cutting
+{
+    class RodCuttingMain
+    {
+        static void Main()
+        {
+            RodCuttingMenu rodCutting = new RodCuttingMenu();
+            rodCutting.ScenarioA();
+            rodCutting.ScenarioB();
+            rodCutting.ScenarioC();
+            
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/metal-factory-pipe-cutting/RodCuttingUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.metal_factory_pipe_cutting
+{
+    class RodCuttingUtility : IRodCuttingStrategy
+    {
+        public int GetMaxProfit(int[] prices, int rodLength)
+        {
+            if (rodLength == 0)
+            {
+                return 0;
+            }
+            int max = int.MinValue;
+            for(int i=1; i<=rodLength; i++)
+            {
+                max = Math.Max(max, prices[i-1] + GetMaxProfit(prices, rodLength - i));
+            }
+            return max;
+        }
+
+        public int GetProfit(int[] prices, int rodLength)
+        {
+            int idx = rodLength / 2 - 1;
+            return prices[idx] * 2;
+        }
+    }
+}

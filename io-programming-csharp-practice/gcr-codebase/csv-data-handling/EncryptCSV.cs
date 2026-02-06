d7f82fec2170c28bd7cc61ca472ad3665f4e7366
Shyam Rai <shyamrrai0011@gmail.com>
2026-02-06 11:51:16 +0530

    [Shyam Rai] Added BasicSqlCommands

--- io-programming-csharp-practice/gcr-codebase/csv-data-handling/EncryptCSV.cs
+++
@@ -1,9 +0,0 @@
-
-using System;
-using System.Text;
-
-class EncryptCSV
-{
-    static string Encrypt(string t)=>Convert.ToBase64String(Encoding.UTF8.GetBytes(t));
-    static string Decrypt(string t)=>Encoding.UTF8.GetString(Convert.FromBase64String(t));
-}
